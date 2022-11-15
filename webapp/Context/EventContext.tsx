1ee397ec648b31d7aa3494dc153af37d5e109643
Nguyen Viet <viet@leansoft-inc.com>
2022-11-15 09:53:44 +0700

    merge branch main

--- webapp/Context/EventContext.tsx
+++ webapp/Context/EventContext.tsx
@@ -1,15 +1,12 @@
-import { Props } from "next/script";
-import React, { useContext } from "react";
-import EventSol from "../../@artifacts/contracts/Event.sol/Event.json";
-import { Event } from "../../@types/contracts/Event";
-import { connectContract, ConnectionContext } from "./ConnectionContext";
-import { ethers } from "ethers";
+import {Props} from 'next/script';
+import React from 'react';
+import EventSol from '../../@artifacts/contracts/Event.sol/Event.json';
+import {Event} from '../../@types/contracts/Event';
+import {connectContract} from './ConnectionContext';
+import {ethers} from 'ethers';
 
 export interface IEventContext {
-  approveEventManager(
-    _provider: ethers.providers.Web3Provider,
-    _address: string
-  ): Promise<void>;
+  approveEventManager(_provider: ethers.providers.Web3Provider, _address: string): Promise<void>;
   createEvent(
     provider: ethers.providers.Web3Provider,
     eventName: string,
@@ -24,33 +21,23 @@ export interface IEventContext {
 
 const ADDRESS_EVENT_CONTRACT = process.env.NEXT_PUBLIC_EVENT_CONTRACT;
 
-export const EventContext = React.createContext<IEventContext>(
-  {} as IEventContext
-);
+export const EventContext = React.createContext<IEventContext>({} as IEventContext);
 
-export const EventProvider: React.FC<Props> = ({ children }) => {
+export const EventProvider: React.FC<Props> = ({children}) => {
   // Aprrove Event Manager Func
-  const approveEventManager = async (
-    _provider: ethers.providers.Web3Provider,
-    _address: string
-  ): Promise<void> => {
-    const contract = connectContract(
-      ADDRESS_EVENT_CONTRACT!,
-      EventSol.abi,
-      _provider.getSigner()
-    ) as Event;
+  const approveEventManager = async (_provider: ethers.providers.Web3Provider, _address: string): Promise<void> => {
+    const contract = connectContract(ADDRESS_EVENT_CONTRACT!, EventSol.abi, _provider.getSigner()) as Event;
 
     try {
       const transaction = await contract!.approveEventManager(_address);
       await transaction.wait();
-      alert("Approve success");
+      alert('Approve success');
     } catch (error: any) {
       console.log(error.message);
       // if()
       // if(error.)
     }
   };
-
 
   // Create event in bockchain
   const createEvent = async (
@@ -65,27 +52,25 @@ export const EventProvider: React.FC<Props> = ({ children }) => {
   ): Promise<void> => {
     const signer = provider.getSigner();
     const contract = connectContract(ADDRESS_EVENT_CONTRACT!, EventSol.abi, signer) as Event;
-    
-    const transaction = await contract.createEvent(
-      eventName,
-      location,
-      description,
-      hashImage,
-      price,
-      startDay,
-      endDay
-    );
-    await transaction.wait();
-    console.log('Transaction: ', transaction);
 
-    const hash = await contract.getString();
-    console.log('Hash: ', hash);
-    
+    try {
+      const transaction = await contract.createEvent(
+        eventName,
+        location,
+        description,
+        hashImage,
+        price,
+        startDay,
+        endDay
+      );
+      await transaction.wait();
+      alert('Create event successfully');
+    } catch (error) {
+      console.error('Error: ', error);
+    }
   };
 
   const value: IEventContext = {approveEventManager, createEvent};
 
-  return (
-    <EventContext.Provider value={value}>{children}</EventContext.Provider>
-  );
+  return <EventContext.Provider value={value}>{children}</EventContext.Provider>;
 };
